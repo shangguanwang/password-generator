7dcfb287af10f15eea77037da28263860207be47
Up Crown <wangshangguan@hotmail.com>
2024-01-25 10:34:52 -0800

    generate two passwords for #1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Random Password Generator
-Click on the "generate passwords" button and it will generate two random passwords for users. Each password is 15 characters long.# password-generator
+Click on the "generate passwords" button and it will generate two random passwords for users. 
+
+Each password is 15 characters long.# password-generator
--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+let password1El = document.getElementById("first-choice");
+let password2El = document.getElementById("second-choice");
+let passwordLength = 15;
+const characters = [
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+    "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d",
+    "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+    "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+    "y", "z", "!", "@", "#", "%", "&", "*",
+];
+
+function generatePassword(){
+    let res = "";
+    for (let i=0; i < passwordLength; i++){
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        res += characters[randomIndex];
+    }
+    return res;
+}
+
+function renderPassword(){
+    password1El.textContent = generatePassword();
+    password2El.textContent = generatePassword();
+}
 
 
 
